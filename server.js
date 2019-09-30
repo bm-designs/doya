a1c3fac36a7b623ad90a96e5763c167133e56466
James Laskey <james.laskey23@gmail.com>
2019-09-29 20:39:13 -0700

    last minute but this is the real real one lol

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var util = require('util');
 // const squareConnect = require('square-connect');
 var app = express();
 // var nodemailer = require('nodemailer');
-var client = squareConnect.ApiClient.instance;
+//var client = squareConnect.ApiClient.instance;
 // client.basePath = 'https://connect.squareupsandbox.com';
 // var oauth2 = client.authentications['oauth2'];
 // oauth2.accessToken = "EAAAECkmJ2Kz2o0AZJTFGfiQQyryl6_q0umjm1YuPNQFx98QJfgqxP5cLyRr1rOi";
@@ -29,56 +29,56 @@ var server = app.listen(PORT || 3000, function() {
 	console.log("lisening on port number %d", server.address().port);
 });
 app.get('/', (req, res) => res.sendFile(__dirname+"/public/index.html"));
-app.post('/process-payment', async (req, res) => {
-  const request_params = req.body;
-  var data = Object.keys(request_params)[0].split('\"');
-  console.log(data)
-  //var params = change.replace('\\/[\\{}"]+/g'," ")
-  var nonce = data[3]
-  var address = data[11]
-  var name = data[7]
-  var size = data[15]
-  var shirt = data[19]
-  var email = data[23]
-  var zipcode = data[27]
+// app.post('/process-payment', async (req, res) => {
+//   const request_params = req.body;
+//   var data = Object.keys(request_params)[0].split('\"');
+//   console.log(data)
+//   //var params = change.replace('\\/[\\{}"]+/g'," ")
+//   var nonce = data[3]
+//   var address = data[11]
+//   var name = data[7]
+//   var size = data[15]
+//   var shirt = data[19]
+//   var email = data[23]
+//   var zipcode = data[27]
 
-  // length of idempotency_key should be less than 45
-  const idempotency_key = crypto.randomBytes(22).toString('hex');
+//   // length of idempotency_key should be less than 45
+//   const idempotency_key = crypto.randomBytes(22).toString('hex');
 
-  // Charge the customer's card
-  const payments_api = new squareConnect.PaymentsApi();
-  const request_body = {
-    source_id: nonce,
-    amount_money: {
-      amount: 3000, // $30.00 charge
-      currency: 'USD'
-    },
-    idempotency_key: idempotency_key
-  };
+//   // Charge the customer's card
+//   const payments_api = new squareConnect.PaymentsApi();
+//   const request_body = {
+//     source_id: nonce,
+//     amount_money: {
+//       amount: 3000, // $30.00 charge
+//       currency: 'USD'
+//     },
+//     idempotency_key: idempotency_key
+//   };
 
-  try {
-    const response = await payments_api.createPayment(request_body);
-    var query = "INSERT INTO doya.orders (name, email, shirt, size, address, zipcode) VALUES($1,$2,$3,$4,$5,$6)"
-    var values = [name, email, shirt, size, address, zipcode]
-    console.log(values)
-    db.query(query,values, function(err, result){
-    	if(err) {
-    		console.log(err)
-    	}
+//   try {
+//     const response = await payments_api.createPayment(request_body);
+//     var query = "INSERT INTO doya.orders (name, email, shirt, size, address, zipcode) VALUES($1,$2,$3,$4,$5,$6)"
+//     var values = [name, email, shirt, size, address, zipcode]
+//     console.log(values)
+//     db.query(query,values, function(err, result){
+//     	if(err) {
+//     		console.log(err)
+//     	}
 
-    })
-    res.status(200).json({
-      'title': 'Payment Successful',
-      'result': response
-    });
-  } catch(error) {
-    res.status(500).json({
-      'title': 'Payment Failure',
-      'result': error.response.text
-    });
-  }
+//     })
+//     res.status(200).json({
+//       'title': 'Payment Successful',
+//       'result': response
+//     });
+//   } catch(error) {
+//     res.status(500).json({
+//       'title': 'Payment Failure',
+//       'result': error.response.text
+//     });
+//   }
 
-});
+// });
 // app.get('/doyasecretlink', function(req,res) {
 //   res.sendFile(__dirname+"/public/components/orders.html")
 // })
